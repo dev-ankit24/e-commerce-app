208860738e6bffeac237a53359cdbe22ca5238a3
dev-ankit24 <ankitkumar839282@gmail.com>
2024-07-04 09:30:32 +0530

    fix bug contactUs Map

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -87,15 +87,14 @@ export default function Contact() {
             style={{minHeight: "450px"}}
           >
             <div className="position-relative h-100">
-              <iframe
-                className="position-relative w-100 h-100"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
-                frameborder="0"
-                style={{minHeight: "450px", border: "0px"}}
-                allowfullscreen=""
-                aria-hidden="false"
-                tabindex="0"
-              ></iframe>
+            <div style={{overflow:"hidden", maxWidth:"100%", width:"500px", height:"400px"}}>
+              <div id="canvas-for-googlemap" style={{height:"100%", width:"100%", maxWidth:"100%"}}>
+                <iframe style={{height:"100%", width:"100%", border:"0"}} frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=a-43,+sector-16+noida&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8">
+                </iframe>
+                </div>
+                <a className="embed-map-html" rel="nofollow" href="https://www.bootstrapskins.com/themes" id="authmaps-data">premium bootstrap themes</a>
+                {/* <style>#canvas-for-googlemap img{max-height:none;max-width:none!important;background:none!important;}</style> */}
+                </div>
             </div>
           </div>
           <div className="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
